8d2cbb2e51ea2a9bdb51a98ff4efe98db5fa5c5f
Rocky-04 <olegtsynkovskiy@gmail.com>
2022-11-18 07:45:06 +0200

    del all

--- orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('checkout/create_order/', CreateOrderView.as_view(), name='create_order'),
-]
--- users/context_processors.py
+++
@@ -1,6 +0,0 @@
-from .forms import SubscriberEmailForm
-
-
-def getting_subscribe_email_form(request):
-    subscriber_email_form = SubscriberEmailForm
-    return locals()
